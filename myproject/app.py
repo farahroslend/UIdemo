f2bbb62df8756e40877a642905e7475975bbb641
farahroslend <farahroslend@gmail.com>
2022-12-21 20:07:15 +0000

    FE UI MVP completed! Pending ability to grab/hold in file, & access its properties

--- myproject/app.py
+++ myproject/app.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+from openpyxl import load_workbook
 import pandas as pd
-	
+
 
 app = Flask("Excel_Web-App")
-	
+
 
 @app.route("/", methods=["GET", "POST"])
 def form():
-	    if request.method == "POST" and "z" in request.form:
-	        x = request.form.get('z')
-	        x = int(x)
-	        dataset = pd.read_excel("data/dummy_data.xlsx")
-	        Stream = dataset["Stream"]
-	        dataset['Input'] = dataset['Input'] * (x/100)
-	        filename="Updated_file.xlsx"
-	        dataset.to_excel(filename)
-	        return "Data has been Updated Successfully in Updated_file.xlsx!! Check it out!!"
-	
+	    if request.method == "POST" or "z" in request.form:
+			# x = request.form.get('z')
+	        # x = int(x)
+	        # dataset = pd.read_excel("data/dummy_data.xlsx")
+	        # Stream = dataset["Stream"]
+	        # dataset['Input'] = dataset['Input'] * (x/100)
+	        # filename="Updated_file.xlsx"
+	        # dataset.to_excel(filename)
+			# return render_template("view_input_data.html", sheet=sheet)
+	        return "PROMPT: File to be absorbed into BE"
+	        
+	        #return "Data has been Updated Successfully in Updated_file.xlsx!! Check it out!!"			
+		
 
-	    return render_template("form.html")
+	    return render_template("form.html") 
